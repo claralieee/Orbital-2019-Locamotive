d06bdd520c78fd92d1b16a8a9303c55a563c9668
kevvc <kevcxy@gmail.com>
2019-07-27 15:51:41 +0800

    Working commit after merge. Remember: What used to be Plans is not Trips, same for Plan and Trip and non capitalized letters!

--- client/js/profile.js
+++ client/js/profile.js
@@ -22,18 +22,18 @@ Template.profile.helpers({
 		}
 	}, 
 
-	userPlans: function() {
+	userTrips: function() {
 		var username = Meteor.user().username;
 		var userId = Meteor.userId();
-		var userPlans = Plans.find({userId: userId}, {sort: {createdAt: -1}});
-		return userPlans;
+		var userTrips = Trips.find({userId: userId}, {sort: {createdAt: -1}});
+		return userTrips;
 	},
 })
 
 Template.profile.events({
 
 	"click #delete-btn": function() {
-		Meteor.call("deletePlan", this._id);
+		Meteor.call("deleteTrip", this._id);
 		Bert.alert("Your Plan Was Deleted", "success", "growl-top-right")
 	},
 	
